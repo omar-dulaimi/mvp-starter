3694e72f58ef262fc76c27407bc70c9d74a7ff14
omar-dulaimi <o.m.dulaimi@gmail.com>
2018-11-25 18:00:20 +0200

    style fixes

--- helpers/faveQsAPI.js
+++ helpers/faveQsAPI.js
@@ -1,50 +1,5 @@
 const request = require('request');
 
-// let getQuoteCategories = async function (callback) {
-//     const options = {
-//         url: 'http://quotes.rest/qod/categories.json',
-//         method: 'GET',
-//         headers: {
-//             'Accept': 'application/json',
-//             'Accept-Charset': 'utf-8',
-//         }
-//     };
-
-//     request(options, function (err, res, body) {
-//         if (err) {
-//             callback(err, null);
-//         } else {
-//             console.log("------------API: getting Categories!")
-//             callback(err, JSON.parse(body))
-//         }
-//     });
-// }
-
-// let getQuoteForACategory = function (category, callback) {
-//     const options = {
-//         url: `http://quotes.rest/qod.json?category=${category}`,
-//         method: 'GET',
-//         headers: {
-//             'Accept': 'application/json',
-//             'Accept-Charset': 'utf-8',
-//         }
-//     };
-
-//     request(options, function (err, res, body) {
-//         if (err) {
-//             callback(err, null);
-//         } else {
-//             console.log("------------API: getting Quote!")
-//             callback(err, JSON.parse(body))
-//         }
-//     });
-// }
-
-
-// module.exports.getQuoteCategories = getQuoteCategories;
-// module.exports.getQuoteForACategory = getQuoteForACategory;
-
-
 let getQuote = function (callback) {
     const options = {
         url: `https://favqs.com/api/qotd`,
@@ -66,7 +21,6 @@ let getQuote = function (callback) {
 }
 
 
-// https://en.wikipedia.org/w/api.php?action=query&prop=pageimages&format=json&piprop=original&titles=Albert%20Einstein
 let getPicture = function (author, callback) {
     const options = {
         url: `http://en.wikipedia.org/w/api.php?action=query&prop=pageimages&format=json&piprop=original&titles=${encodeURI(author)}`,
