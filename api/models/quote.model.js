91b951c80b19d46b8496ffcd330890b953491220
Omar Dulaimi <o.m.dulaimi@gmail.com>
2019-07-27 16:35:35 +0300

    add quote model

---
+++ api/models/quote.model.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const Schema = require('../config').Schema;
+
+const QuoteSchema = new Schema(
+    {
+        quote: {
+            type: String,
+            required: true,
+        },
+        author: {
+            type: String,
+            required: true,
+        },
+        isDeleted: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    {
+        timestamps: true,
+    },
+);
+
+// this is used to remove both _id and _v, returning id instead
+QuoteSchema.set('toJSON', {
+    virtuals: true,
+    versionKey: false,
+    transform: (doc, ret) => {
+        ret.id = ret._id;
+        delete ret._id;
+        return ret;
+    },
+});
+
+module.exports = mongoose.model('Quote', QuoteSchema);
