22916d4885e55d6ab9318518eb54c89e80a921f1
Omar Dulaimi <o.m.dulaimi@gmail.com>
2019-07-27 17:13:29 +0300

    implement generate quote

--- api/controllers/quote.controller.js
+++ api/controllers/quote.controller.js
@@ -1,5 +1,48 @@
 const Quote = require('../models/quote.model');
+const axios = require('axios');
 
-exports.generateQuote = (req, res) => {};
+function getAuthorImage(wikiObj) {
+    const {
+        query: { pages },
+    } = wikiObj;
 
-exports.addQuoteToFavs = (req, res) => {};
+    let authorImage = null;
+
+    for (const key in pages) {
+        if (
+            pages[key].hasOwnProperty('original') &&
+            pages[key].original.hasOwnProperty('source') &&
+            pages[key].original.hasOwnProperty('width') &&
+            pages[key].original.hasOwnProperty('height')
+        ) {
+            authorImage = pages[key].original.source;
+        }
+    }
+
+    return [authorImage];
+}
+
+exports.generateQuote = async (req, res) => {
+    try {
+        const {
+            quote: { body, author },
+        } = await axios.get('https://favqs.com/api/qotd');
+
+        const wikiObj = await axios.get(
+            `http://en.wikipedia.org/w/api.php?action=query&prop=pageimages&format=json&piprop=original&titles=${encodeURI(
+                author,
+            )}`,
+        );
+
+        const [authorImage] = getAuthorImage(wikiObj);
+
+        res.status(200).json({ quote: body, authorImage });
+    } catch (e) {
+        res.status(500).json({
+            message: 'Could not generate quote!',
+            error: e,
+        });
+    }
+};
+
+exports.addQuoteToFavs = async (req, res) => {};
