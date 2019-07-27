76f4619971fe379ab700e4b06729fa2b6b05b85c
Omar Dulaimi <o.m.dulaimi@gmail.com>
2019-07-27 21:47:37 +0300

    fix not serving static files

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const bodyParser = require('body-parser');
 require('dotenv').config();
 
 const routes = require('./api/routes/v1')();
-
 const app = express();
+
 const port = process.env.PORT || 5000;
 app.set('port', port);
 
@@ -15,10 +15,10 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, '/../react-client/dist')));
+app.use(express.static(path.join(__dirname, './react-client/dist')));
 
 app.use('/api/v1', routes);
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port}!\nhttp://localhost:${port}`);
+    console.log(`Listening on port ${port}!\nGo to: http://localhost:${port}`);
 });
