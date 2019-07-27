8775bbff484d114394d6e3135a340fb402d475f0
Omar Dulaimi <o.m.dulaimi@gmail.com>
2019-07-27 17:49:03 +0300

    refactor server

--- server/index.js
+++ server/index.js
@@ -1,73 +1,24 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
 
-var db = require('../database-mongo');
-var api = require('../helpers/faveQsAPI');
+require('dotenv').config();
 
-var app = express();
-app.use(bodyParser.json())
-app.use(express.static(__dirname + '/../react-client/dist'));
+const routes = require('./api/routes/v1')();
 
-var port = process.env.PORT || 5000;
+const app = express();
+const port = process.env.PORT || 5000;
+app.set('port', port);
 
-//Get a quote from API and send it back to the client
-app.get('/items', function (req, res) {
-  api.getQuote(function (err, data) {
-    let quote = data.quote.body;
-    let author = data.quote.author;
-    api.getPicture(author, function (err, result) {
-      let obj = result.query.pages;
-      let imageUrl;
-      for (let key in obj) {
-        if (obj[key].hasOwnProperty('original') && obj[key].original.hasOwnProperty('source')) {
-          imageUrl = obj[key].original.source;
-          console.log('--------imgurl = ', imageUrl)
-        }
-      }
-      imageUrl ? res.send({ quote: quote, author: author, imageUrl: imageUrl }) : res.send({ quote: quote, author: author });
-    })
-  });
-});
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, '/../react-client/dist')));
 
-//Save a quote sent from the client on the DB
-app.post('/favquotes', function (req, res) {
-  let quote = req.body.quote;
-  let author = req.body.author;
+app.use('/api/v1', routes);
 
-  //Make sure this quote was not posted before
-  db.isQuoteExist(quote, function (err, result) {
-    if (err) {
-      console.log('^---error:POST checking for quote!');
-    } else {
-      if (!result) {
-        db.insertQuote(quote, author, function (err, result) {
-          if (err) {
-            console.log('^---error:POST:Insert in inserting quote!');
-          } else {
-            res.send('<---Quote Saved on DB!--->');
-          }
-        });
-      } else {
-        res.send('^---Quote already exist!---^');
-        console.log('^---Quote already exist!---^');
-      }
-    }
-  });
-});
-
-//Send the quotes saved on the DB back to the client
-app.get('/favquotes', function (req, res) {
-  db.selectAll(function (err, data) {
-    if (err) {
-      console.log('^---error:GET:selectAll:favquotes!')
-      res.sendStatus(500);
-    } else {
-      console.log('<---Sending favquotes from DB to Client--->');
-      res.send(data);
-    }
-  });
-});
-
-app.listen(port, function () {
-  console.log('listening on port 5000!');
+app.listen(port, () => {
+    console.log(`Listening on port ${port}!\nhttp://localhost:${port}`);
 });
