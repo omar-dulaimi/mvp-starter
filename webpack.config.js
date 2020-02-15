6c1f42b552a4542662f4b7b2a17e0d9e78bc27a3
Omar Dulaimi <omar@eila.io>
2020-02-15 13:14:24 +0200

    refactor webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,49 @@
-var path = require('path');
-var SRC_DIR = path.join(__dirname, '/client/src');
-var DIST_DIR = path.join(__dirname, '/client/dist');
+const path = require('path');
+const SRC_DIR = path.join(__dirname, '/client/src');
+const DIST_DIR = path.join(__dirname, '/client/dist');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
+const config = {
     entry: `${SRC_DIR}/index.jsx`,
     output: {
-        filename: 'bundle.js',
         path: DIST_DIR,
+        filename: 'bundle.js'
     },
     module: {
-        loaders: [
+        rules: [
             {
-                test: /\.jsx?/,
-                include: SRC_DIR,
-                loader: 'babel-loader',
-                query: {
-                    presets: ['react', 'es2015'],
-                },
+                test: /\.(js|jsx)$/,
+                include: path.resolve(__dirname, 'client/src'),
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
             },
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader'
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader"
+                    }
+                ]
+            }
         ],
     },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./client/src/index.html",
+            filename: "./index.html"
+        }),
+        new MiniCssExtractPlugin({ // plugin for controlling how compiled css will be outputted and named
+            filename: "client/dist/[name].css",
+            chunkFilename: "css/[id].css"
+        })
+    ]
 };
+
+module.exports = config;
