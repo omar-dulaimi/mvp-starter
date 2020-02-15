313b49a77c6d4be5f9aa2405a17b6cf6283ccd8a
Omar Dulaimi <omar@eila.io>
2020-02-15 13:14:59 +0200

    refactor styles ans components

--- client/src/components/Favs/Favs.jsx
+++ client/src/components/Favs/Favs.jsx
@@ -1,19 +1,17 @@
 import React from 'react';
 
 const Favs = (props) => {
-  var result = props.favquotes.map((fav, index) =>
-    <div key={index}>
+  var result = props.favquotes.map((fav, index) => (
+    <div key={index} className="favs">
       <blockquote className="blockquote">
         <p className="mb-0 quote">{fav.quote}</p>
-        <footer className="blockquote-footer author text-dark">{fav.author}</footer>
+        <footer className="blockquote-footer author text-dark">
+          {fav.author}
+        </footer>
       </blockquote>
     </div>
-  )
+  ));
 
-  return (
-    <div>
-      {result}
-    </div>
-  )
-}
+  return <div>{result}</div>;
+};
 export default Favs;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,126 +1,133 @@
+import './styles.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
+
 import Favs from './components/Favs/Favs.jsx';
 import QuoteImage from './components/Quotes/QuoteImage/QuoteImage.jsx';
 import Quotes from './components/Quotes/Quotes.jsx';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            quote: [],
-            favquotes: [],
-            showFavs: false,
-            showImage: false,
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      quote: [],
+      favquotes: [],
+      showFavs: false,
+      showImage: false,
+    };
+  }
 
-    componentDidMount() {
-        this.generateQuote();
-    }
+  componentDidMount() {
+    this.generateQuote();
+  }
 
-    generateQuote() {
-        if (this.state.showFavs) {
-            this.setState({ showFavs: false });
+  generateQuote() {
+    if (this.state.showFavs) {
+      this.setState({ showFavs: false });
+    }
+    this.setState({ showImage: true });
+
+    axios
+      .get('/api/v1/quotes/generate')
+      .then((res) => {
+        this.setState({
+          quote: res.data,
+        });
+      })
+      .catch((err) => {
+        console.log({
+          msg: 'Failure: could not generate quote!',
+          error: err,
+        });
+      });
+  }
+
+  addToFavoriteQuotes() {
+    const { quote } = this.state;
+    axios
+      .post('/api/v1/quotes', quote)
+      .then((res) => {})
+      .catch((err) => {
+        console.log({
+          msg: 'Failure: could not send quote!',
+          error: err,
+        });
+      });
+  }
+
+  retrieveFavorites() {
+    const { showImage, showFavs } = this.state;
+    axios
+      .get('/api/v1/quotes')
+      .then((favquotes) => {
+        this.setState({
+          favquotes: favquotes.data,
+        });
+
+        if (showImage) {
+          this.setState({ showImage: false });
         }
-        this.setState({ showImage: true });
+        this.setState({ showFavs: !showFavs });
+      })
+      .catch((err) => {
+        console.log({
+          msg: 'Failure: could not get favorites!',
+          error: err,
+        });
+      });
+  }
 
-        axios
-            .get('/api/v1/quotes/generate')
-            .then((res) => {
-                this.setState({
-                    quote: res.data,
-                });
-            })
-            .catch((err) => {
-                console.log({
-                    msg: 'Failure: could not generate quote!',
-                    error: err,
-                });
-            });
-    }
-
-    addToFavoriteQuotes() {
-        const { quote } = this.state;
-        axios
-            .post('/api/v1/quotes', quote)
-            .then((res) => {})
-            .catch((err) => {
-                console.log({
-                    msg: 'Failure: could not send quote!',
-                    error: err,
-                });
-            });
-    }
-
-    retrieveFavorites() {
-        const { showImage, showFavs } = this.state;
-        axios
-            .get('/api/v1/quotes')
-            .then((favquotes) => {
-                this.setState({
-                    favquotes: favquotes.data,
-                });
-
-                if (showImage) {
-                    this.setState({ showImage: false });
-                }
-                this.setState({ showFavs: !showFavs });
-            })
-            .catch((err) => {
-                console.log({
-                    msg: 'Failure: could not get favorites!',
-                    error: err,
-                });
-            });
-    }
-
-    render() {
-        return (
-            <div className="container h-100">
-                <div className="row h-100 justify-content-center align-items-center contentainer">
-                    <div className="col-md-12">
-                        <h1 className="title">Quotes Machine</h1>
-                    </div>
-                    <div className="col-md-12">
-                        <Quotes quote={this.state.quote} />
-                    </div>
-                    <div className="col-md-8 text-center">
-                        <button
-                            type="button"
-                            className="btn btn-success"
-                            onClick={this.generateQuote.bind(this)}
-                        >
-                            Generate
-                        </button>
-                        <button
-                            type="button"
-                            className="btn btn-primary"
-                            onClick={this.addToFavoriteQuotes.bind(this)}
-                        >
-                            Like
-                        </button>
-                        <button
-                            type="button"
-                            className="btn btn-info"
-                            onClick={this.retrieveFavorites.bind(this)}
-                        >
-                            Favorites
-                        </button>
-                    </div>
-                </div>
-                <div className="row h-80 justify-content-center align-items-center contentainerfavs">
-                    {this.state.showFavs ? (
-                        <Favs favquotes={this.state.favquotes} />
-                    ) : null}
-                    {this.state.showImage ? (
-                        <QuoteImage quoteImg={this.state.quote.authorImage} />
-                    ) : null}
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="container h-100">
+        <div className="row h-100 justify-content-center align-items-center contentainer">
+          <div className="col-md-12">
+            <h1 className="title">Quotes Machine</h1>
+          </div>
+          <div className="col-md-12">
+            <Quotes quote={this.state.quote} />
+          </div>
+          <div className="col-md-8 text-center">
+            <button
+              type="button"
+              className="btn btn-success"
+              onClick={this.generateQuote.bind(this)}
+            >
+              Generate
+            </button>
+            <button
+              type="button"
+              className="btn btn-primary"
+              onClick={this.addToFavoriteQuotes.bind(this)}
+            >
+              Like
+            </button>
+            <button
+              type="button"
+              className="btn btn-info"
+              onClick={this.retrieveFavorites.bind(this)}
+            >
+              Favorites
+            </button>
+          </div>
+        </div>
+        <div className="row h-80 justify-content-center align-items-center favs">
+          {this.state.showFavs ? (
+            <Favs favquotes={this.state.favquotes} />
+          ) : null}
+          {this.state.showImage ? (
+            <QuoteImage quoteImg={this.state.quote.authorImage} />
+          ) : null}
+        </div>
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
